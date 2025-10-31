7cf9f36f9f44cd34d30dde1c494478939270bc40
Valentino Privitera <devalen543@gmail.com>
2025-10-31 16:38:19 -0300

    Rama para tests invocada

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,10 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
+// createClient() crea el cliente con la URL de la base.
 const client = createClient({ url: process.env.DATABASE_URL! });
+
+// drizzle({ client }) inicializa la conexión que luego se usa en todo el proyecto.
 const db = drizzle({ client });
 
 export default db;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,7 @@
 import { int, sqliteTable, text, real } from 'drizzle-orm/sqlite-core';
 
+// Define el esquema de una tabla de la BDD, en este caso representa la de productos
+
 export const productsTable = sqliteTable('products_table', {
   id: int().primaryKey({ autoIncrement: true }),
   user_id: text().notNull(),
